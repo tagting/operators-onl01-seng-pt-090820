1c07eaebce8f90d828c2347564306822ac5d51b9
Ting Ting Zheng <ttzhenng@gmail.com>
2020-09-21 21:29:54 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
 def unsafe?(speed)
-
+  if speed < 40
+    return true
+    
+  elsif speed > 60
+    return true
+  
+  else 
+    return false
+  end
 end
 
 
 
-def not_safe?(speed)
-	
-end
+#def not_safe?(speed) #ternary operator
+	#speed < 40 && > 60 FALSE : FALSE
+#end
 	
 
 
